667effd2f19744c9cd2aa40aa780e0216047bc4d
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2016-12-05 18:00:09 +1300

    Test server ready for experiments to be written.

--- test_server.py
+++ test_server.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import socket
 import sys
 from test_logger import TestLogger
@@ -84,6 +85,9 @@ class TestServer:
     _HOST = ""  # Symbolic for all available interfaces
     _PORT = 8088
 
+    # Other constants
+    _ANS_PLYBK = "ansible-playbook ~/AutomatedTests/playbooks/"
+
     def __init__(self, log_file):
         """Initialise.
 
@@ -93,9 +97,37 @@ class TestServer:
         self._log_file = log_file
         self._logger = TestLogger("TestServer", self._log_file)
 
-    def start_experiments(self):
+    def start_experiments(self, exp_data):
         """Start a set of experiments.
+
+        :param exp_data: Dict containing parameters for the experiments.
         """
+        self._logger.info("Starting experiment.")
+
+        self._logger.info("Killing any existing evaluation processes "
+                          "on testbed hosts.")
+        playbook_cmd = self._ANS_PLYBK + "copy_config_policy.yaml " \
+                       "--extra-vars \"contr_config={0} " + \
+                       "contr_main_policy={1} dpae_dataset={2}\"" + \
+                       "".format(exp_data["contr_config"], exp_data[
+                           "contr_main_policy"], exp_data[
+                           "dpae_dataset"])
+        os.system(playbook_cmd)
+
+        self._logger.info("Copying files to the controller and DPAE.")
+        playbook_cmd = self._ANS_PLYBK + "start_controller.yaml"
+        os.system(playbook_cmd)
+
+        self._logger.info("Starting controller application.")
+        playbook_cmd = self._ANS_PLYBK + "start_controller.yaml"
+        os.system(playbook_cmd)
+
+        self._logger.info("Starting DPAE application.")
+        playbook_cmd = self._ANS_PLYBK + "start_dpae.yaml " + \
+                       "--extra-vars \"mosp_outfile={0}\"".format(
+                           exp_data["mosp_outfile"])
+        os.system(playbook_cmd)
+
         self._logger.info("Waiting for DPAE notification...")
         server = DPAEHandler(self._log_file)
         result = server.handle(self._HOST, self._PORT)
@@ -104,8 +136,20 @@ class TestServer:
                                   "notification failed. Exiting...")
             sys.exit(1)
 
+        self._logger.info("Starting traffic replay.")
+        playbook_cmd = self._ANS_PLYBK + "start_replay.yaml"
+        os.system(playbook_cmd)
+
+        self._logger.info("Killing all running evaluation processes "
+                          "on testbed hosts.")
+        playbook_cmd = self._ANS_PLYBK + "kill_processes.yaml"
+        os.system(playbook_cmd)
+
+        self._logger.info("Test server shutting down...")
+
 
 if __name__ == "__main__":
     log_file = "test_server.log"
     ts = TestServer(log_file)
-    ts.start_experiments()
+    exp_data = {}
+    ts.start_experiments(exp_data)
